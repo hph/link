98a6c54f1b85bc1d5dadee4b50d5835978f24aa9
Haukur Páll Hallvarðsson <haukurpallh@gmail.com>
2014-05-17 05:52:54 +0200

    Use localStorage to store links that have been created

--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,22 @@
 $(function() {
+  _.templateSettings = {interpolate: /\{\{(.+?)\}\}/g}
+  var links = JSON.parse(localStorage.getItem('links'));
+  if (links === null) {
+    links = [];
+  } else {
+    $('#toggle-links').show();
+    _.each(links, function(link) {
+      $('.old-links').prepend(_.template($('#link-template').html(), link));
+    });
+  }
+
+  function add_link(link) {
+    $('.old-links').prepend(_.template($('#link-template').html(), link));
+    links.push(link);
+    localStorage.setItem('links', JSON.stringify(links));
+    $('#toggle-links').show();
+  }
+
   function post() {
     if ($('#long-url').val().length === 0) {
       return alert('The text field is empty...');
@@ -16,6 +34,7 @@ $(function() {
           var key = 'Control';
         }
         $('#info-text').html('Press ' + key + '-C to copy the URL');
+        add_link(response.link);
       },
       dataType: 'json'
     });
@@ -28,6 +47,18 @@ $(function() {
   $('#long-url').keypress(function(event) {
     if (event.keyCode === 13) {
       post();
+    }
+  });
+
+  $('#toggle-links').click(function() {
+    var button = $('#toggle-links');
+    var links = $('.old-links');
+    if (button.html() === 'Show old links') {
+      button.html('Hide old links');
+      links.show();
+    } else {
+      button.html('Show old links');
+      links.hide();
     }
   });
 });
